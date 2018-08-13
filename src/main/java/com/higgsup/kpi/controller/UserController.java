959d81dc72d9b1aff1f5de0fd8add8f0d31690d4
thanhlv <lethanh9398@gmail.com>
2018-08-13 16:04:03 +0700

    delete api get role, has in get info

--- src/main/java/com/higgsup/kpi/controller/UserController.java
+++ src/main/java/com/higgsup/kpi/controller/UserController.java
@@ -1,106 +1,98 @@
 package com.higgsup.kpi.controller;
 
-import java.util.*;
-
+import com.higgsup.kpi.configure.BaseConfiguration;
 import com.higgsup.kpi.dto.Response;
+import com.higgsup.kpi.dto.UserDTO;
 import com.higgsup.kpi.glossary.ErrorCode;
+import com.higgsup.kpi.service.LdapUserService;
+import com.higgsup.kpi.util.UtilsValidate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.higgsup.kpi.configure.BaseConfiguration;
-import com.higgsup.kpi.dto.UserDTO;
-import com.higgsup.kpi.service.LdapUserService;
-import com.higgsup.kpi.util.UtilsValidate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class UserController {
 
-	@Autowired
-	private LdapUserService ldapUserService;
+    @Autowired
+    private LdapUserService ldapUserService;
 
-	@PreAuthorize("hasAnyRole('EMPLOYEE')")
-	@GetMapping(BaseConfiguration.BASE_API_URL + "/user-info")
-	public @ResponseBody Response getUserInfo() {
-		Response response = new Response(200);
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String username = auth.getName();
-		if (username != null && !username.equals("")) {
-			if (!UtilsValidate.containRegex(username)) {
-				UserDTO user = ldapUserService.getUserDetail(username);
-				if (user != null) {
-					response.setData(user);
-				} else {
-					response.setMessage(ErrorCode.NOT_FIND_USER.getContent());
-					response.setStatus(ErrorCode.NOT_FIND_USER.getValue());
-				}
-			}
-		}
-		return response;
-	}
+    @PreAuthorize("hasAnyRole('EMPLOYEE')")
+    @GetMapping(BaseConfiguration.BASE_API_URL + "/user-info")
+    public @ResponseBody
+    Response getUserInfo() {
+        Response response = new Response(200);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        if (username != null && !username.equals("")) {
+            if (!UtilsValidate.containRegex(username)) {
+                UserDTO user = ldapUserService.getUserDetail(username);
+                if (user != null) {
+                    response.setData(user);
+                } else {
+                    response.setMessage(ErrorCode.NOT_FIND_USER.getContent());
+                    response.setStatus(ErrorCode.NOT_FIND_USER.getValue());
+                }
+            }
+        }
+        return response;
+    }
 
-	@PreAuthorize("hasRole('ADMIN')")
-	@GetMapping(BaseConfiguration.BASE_API_URL + "/users")
-	public @ResponseBody Response getListUsers() {
-		Response response = new Response(200);
-		List<UserDTO> listUsers = ldapUserService.getAllUsers();
-		if (!listUsers.isEmpty()) {
-			response.setData(listUsers);
-			response.setMessage(HttpStatus.OK.getReasonPhrase());
-		} else {
-			response.setMessage(ErrorCode.NOT_FIND_USER.getContent());
-			response.setStatus(ErrorCode.NOT_FIND_USER.getValue());
-		}
-		return response;
-	}
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping(BaseConfiguration.BASE_API_URL + "/users")
+    public @ResponseBody
+    Response getListUsers() {
+        Response response = new Response(200);
+        List<UserDTO> listUsers = ldapUserService.getAllUsers();
+        if (!listUsers.isEmpty()) {
+            response.setData(listUsers);
+            response.setMessage(HttpStatus.OK.getReasonPhrase());
+        } else {
+            response.setMessage(ErrorCode.NOT_FIND_USER.getContent());
+            response.setStatus(ErrorCode.NOT_FIND_USER.getValue());
+        }
+        return response;
+    }
 
-	@PreAuthorize("hasRole('ADMIN')")
-	@PutMapping(BaseConfiguration.BASE_API_URL + "/user-role/{username}")
-	public @ResponseBody List<Map<String, Object>> updateUserRole(@RequestBody Map<String, Object> context) {
-		String newRole = (String) context.get("role");
-		return null;
-	}
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping(BaseConfiguration.BASE_API_URL + "/user-role/{username}")
+    public @ResponseBody
+    List<Map<String, Object>> updateUserRole(@RequestBody Map<String, Object> context) {
+        String newRole = (String) context.get("role");
+        return null;
+    }
 
-	@PreAuthorize("hasRole('ADMIN')")
-	@GetMapping(BaseConfiguration.BASE_API_URL + "/user-role/{username}")
-	public @ResponseBody Map<String, Object> getRole(@PathVariable String username) {
-		Map<String, Object> result = new HashMap<String, Object>();
-		UserDTO user = ldapUserService.getUserDetail(username);
-		result.put("role", user.getUserRole());
-		return result;
-	}
-
-	@PreAuthorize("hasRole('ADMIN')")
-	@GetMapping(BaseConfiguration.BASE_API_URL + "/search-users/{name}")
-	public @ResponseBody Response searchUsers(@PathVariable String name) {
-		Response response = new Response(200);
-		if (name != null && !name.equals("")) {
-			if (!UtilsValidate.containRegex(name)) {
-				List<UserDTO> listUsers = ldapUserService.findUsersByName(name);
-				if (!listUsers.isEmpty()) {
-					response.setData(listUsers);
-					response.setMessage(HttpStatus.OK.getReasonPhrase());
-				} else {
-					response.setMessage(ErrorCode.NOT_FIND_USER.getContent());
-					response.setStatus(ErrorCode.NOT_FIND_USER.getValue());
-				}
-			} else {
-				response.setMessage(ErrorCode.PARAMETERS_IS_NOT_VALID.getContent());
-				response.setStatus(ErrorCode.PARAMETERS_IS_NOT_VALID.getValue());
-			}
-		} else {
-			response.setMessage(ErrorCode.PARAMETERS_IS_MISSING.getContent());
-			response.setStatus(ErrorCode.PARAMETERS_IS_MISSING.getValue());
-		}
-		return response;
-	}
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping(BaseConfiguration.BASE_API_URL + "/search-users/{name}")
+    public @ResponseBody
+    Response searchUsers(@PathVariable String name) {
+        Response response = new Response(200);
+        if (name != null && !name.equals("")) {
+            if (!UtilsValidate.containRegex(name)) {
+                List<UserDTO> listUsersByName = ldapUserService.findUsersByName(name);
+                if (!listUsersByName.isEmpty()) {
+                    response.setData(listUsersByName);
+                    response.setMessage(HttpStatus.OK.getReasonPhrase());
+                } else {
+                    response.setMessage(ErrorCode.NOT_FIND_USER.getContent());
+                    response.setStatus(ErrorCode.NOT_FIND_USER.getValue());
+                }
+            } else {
+                response.setMessage(ErrorCode.PARAMETERS_IS_NOT_VALID.getContent());
+                response.setStatus(ErrorCode.PARAMETERS_IS_NOT_VALID.getValue());
+            }
+        } else {
+            response.setMessage(ErrorCode.PARAMETERS_IS_MISSING.getContent());
+            response.setStatus(ErrorCode.PARAMETERS_IS_MISSING.getValue());
+        }
+        return response;
+    }
 
 }
--- src/main/java/com/higgsup/kpi/service/impl/LdapUserServiceImpl.java
+++ src/main/java/com/higgsup/kpi/service/impl/LdapUserServiceImpl.java
@@ -19,51 +19,49 @@ import static org.springframework.ldap.query.LdapQueryBuilder.query;
 @Service
 public class LdapUserServiceImpl implements LdapUserService {
 
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
-	@Autowired
-	private LdapTemplate ldapTemplate;
+    @Autowired
+    private LdapTemplate ldapTemplate;
 
-	@Override
-	public UserDTO getUserDetail(String username) {
-		UserDTO user = new UserDTO();
-		LdapQuery query = query().where("objectclass").is("user").and("sAMAccountName").is(username);
-		try {
-			List<UserDTO> queryResult = ldapTemplate.search(query, new UserAttributesMapper());
-			if (!queryResult.isEmpty()) {
-				user = queryResult.get(0);
-			}
-		} catch (Exception ex) {
+    @Override
+    public UserDTO getUserDetail(String username) {
+        UserDTO user = new UserDTO();
+        LdapQuery query = query().where("objectclass").is("user").and("sAMAccountName").is(username);
+        List<UserDTO> queryResult = ldapTemplate.search(query, new UserAttributesMapper());
+        if (!queryResult.isEmpty()) {
+            user = queryResult.get(0);
+            return user;
+        } else {
+            return null;
+        }
+    }
 
-		}
-		return user;
-	}
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<UserDTO> result = null;
+        LdapQuery query = query().where("objectclass").is("user").and("roleForKpi").isPresent();
+        result = ldapTemplate.search(query, new UserAttributesMapper());
+        return result;
+    }
 
-	@Override
-	public List<UserDTO> getAllUsers() {
-		List<UserDTO> result = null;
-		LdapQuery query = query().where("objectclass").is("user").and("roleForKpi").isPresent();
-		result = ldapTemplate.search(query, new UserAttributesMapper());
-		return result;
-	}
+    @Override
+    public UserDTO updateUserRole(String username, List<String> role) {
+        UserDTO user = getUserDetail(username);
+        Name dn = UtilsLdap.buildDn(user, env.getProperty("ldap.baseDN"));
+        DirContextOperations context = ldapTemplate.lookupContext(dn);
+        context.setAttributeValue("roleForKPI", role);
+        return null;
+    }
 
-	@Override
-	public UserDTO updateUserRole(String username, List<String> role) {
-		UserDTO user = getUserDetail(username);
-		Name dn = UtilsLdap.buildDn(user, env.getProperty("ldap.baseDN"));
-		DirContextOperations context = ldapTemplate.lookupContext(dn);
-		context.setAttributeValue("roleForKPI", role);
-		return null;
-	}
-
-	@Override
-	public List<UserDTO> findUsersByName(String name) {
-		List<UserDTO> result = null;
-		LdapQuery query = query().where("objectclass").is("user").and("roleForKpi").isPresent().and("displayName")
-				.like("*" + name + "*");
-		result = ldapTemplate.search(query, new UserAttributesMapper());
-		return result;
-	}
+    @Override
+    public List<UserDTO> findUsersByName(String name) {
+        List<UserDTO> result = null;
+        LdapQuery query = query().where("objectclass").is("user").and("roleForKpi").isPresent().and("displayName")
+                .like("*" + name + "*");
+        result = ldapTemplate.search(query, new UserAttributesMapper());
+        return result;
+    }
 
 }
