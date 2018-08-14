3c70ef052773bcde08d06f2c8401f751c5f9dff5
thanhlv <lethanh9398@gmail.com>
2018-08-14 11:11:49 +0700

    revert code and delete errorCode in Response and add BaseDTO + ErrorMessage

---
+++ src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
@@ -0,0 +1,6 @@
+package com.higgsup.kpi.glossary;
+
+public final class ErrorMessage {
+    public final static String NOT_FIND_USER = "not find user";
+    public final static String PARAMETERS_NAME_IS_NOT_VALID = "parameter name is not valid";
+}
