fbc5bd1d935ea9e309eeb6739d8d5ad1662a514b
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-08-15 15:23:49 +0700

    Project entity + Get all project

---
+++ src/main/java/com/higgsup/kpi/dto/ProjectDTO.java
@@ -0,0 +1,24 @@
+package com.higgsup.kpi.dto;
+
+
+public class ProjectDTO {
+    private String name;
+    private Integer active;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
+
+}
