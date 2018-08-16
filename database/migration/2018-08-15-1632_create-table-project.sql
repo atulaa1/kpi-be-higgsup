f1fa3b2a0ff2f0f0b2cdffd1aee486c22b8375f2
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-08-16 08:39:57 +0700

    migration sql

--- database/migration/2018-08-15-1632_create-table-project.sql
+++ database/migration/2018-08-15-1632_create-table-project.sql
@@ -1,6 +1,6 @@
 CREATE TABLE kpi_project (
-    id int,
-    name varchar(255),
-    active int,
-    created_date timestamp
+  id int(11) NOT NULL AUTO_INCREMENT,
+  active tinyint(4) NULL DEFAULT 1,
+  created_date datetime(0) NULL DEFAULT NULL,
+  name varchar(255)
 );
