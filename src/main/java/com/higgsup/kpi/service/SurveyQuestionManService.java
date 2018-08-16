0bd76a1c98969173e4826ac7290cedef27652b62
Thieu Tu <tu.thieuthanh@gmail.com>
2018-08-16 14:06:48 +0700

    create updateSurveyQuestionMan

--- src/main/java/com/higgsup/kpi/service/SurveyQuestionManService.java
+++ src/main/java/com/higgsup/kpi/service/SurveyQuestionManService.java
@@ -1,9 +1,11 @@
 package com.higgsup.kpi.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.higgsup.kpi.dto.SurveyQuestionManDTO;
 
 import java.util.List;
 
 public interface SurveyQuestionManService {
     List<SurveyQuestionManDTO> getAllQuestion();
+    SurveyQuestionManDTO updateSurveyQuestionMan(SurveyQuestionManDTO surveyQuestionManDTO) throws JsonProcessingException;
 }
