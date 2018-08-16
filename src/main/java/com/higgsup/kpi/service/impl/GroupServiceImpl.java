b363a3ad1324416557dfaba0c088fe49624967a4
Thieu Tu <tu.thieuthanh@gmail.com>
2018-08-16 16:39:23 +0700

    validate point and minimum number of sessions

--- src/main/java/com/higgsup/kpi/service/impl/GroupServiceImpl.java
+++ src/main/java/com/higgsup/kpi/service/impl/GroupServiceImpl.java
@@ -40,13 +40,13 @@ public class GroupServiceImpl implements GroupService {
             if(groupDTO.getName().length()== 0 || groupDTO.getAdditionalConfig().getHost().length() == 0 ){
                 groupDTO1.setMessage(ErrorMessage.PARAMETERS_NAME_IS_NOT_VALID);
                 groupDTO1.setErrorCode(ErrorCode.NOT_NULL.getValue());
-            } else if (minNumberOfSessions != (int) minNumberOfSessions || String.valueOf(minNumberOfSessions).length() > 2 || String.valueOf(minNumberOfSessions).length()==0) {
+            } else if (minNumberOfSessions != (int) minNumberOfSessions || minNumberOfSessions <= 0 || String.valueOf(minNumberOfSessions).length() > 2 || String.valueOf(minNumberOfSessions).length()==0) {
                 groupDTO1.setMessage(ErrorMessage.PARAMETERS_MIN_NUMBER_OF_SESSIONS_IS_NOT_VALID);
                 groupDTO1.setErrorCode(ErrorCode.PARAMETERS_IS_NOT_VALID.getValue());
-            } else if (participationPoint != (float) participationPoint || String.valueOf(participationPoint).substring(1).length() != 2 || String.valueOf(participationPoint).length() == 0) {
+            } else if (participationPoint != (float) participationPoint || participationPoint < 0 || String.valueOf(participationPoint).length() == 0) {
                 groupDTO1.setMessage(ErrorMessage.PARAMETERS_POINT_IS_NOT_VALID);
                 groupDTO1.setErrorCode(ErrorCode.PARAMETERS_IS_NOT_VALID.getValue());
-            } else if (effectivePoint != (float) effectivePoint || String.valueOf(effectivePoint).substring(1).length() != 2 || String.valueOf(effectivePoint).length() == 0) {
+            } else if (effectivePoint != (float) effectivePoint || effectivePoint < 0 || String.valueOf(effectivePoint).length() == 0) {
                 groupDTO1.setMessage(ErrorMessage.PARAMETERS_POINT_IS_NOT_VALID);
                 groupDTO1.setErrorCode(ErrorCode.PARAMETERS_IS_NOT_VALID.getValue());
             } else {
