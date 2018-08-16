45cbd48490b566293329dd519c85ff6a9c69495b
Thieu Tu <tu.thieuthanh@gmail.com>
2018-08-16 14:09:30 +0700

    edit controller

--- src/main/java/com/higgsup/kpi/controller/SurveyQuestionController.java
+++ src/main/java/com/higgsup/kpi/controller/SurveyQuestionController.java
@@ -1,5 +1,6 @@
 package com.higgsup.kpi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.higgsup.kpi.configure.BaseConfiguration;
 import com.higgsup.kpi.dto.Response;
 import com.higgsup.kpi.dto.SurveyQuestionManDTO;
@@ -8,9 +9,7 @@ import com.higgsup.kpi.service.SurveyQuestionManService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -28,6 +27,25 @@ public class SurveyQuestionController {
         Response response = new Response(HttpStatus.OK.value());
         List<SurveyQuestionManDTO> surveyQuestionManDTOS = surveyQuestionManService.getAllQuestion();
         response.setData(surveyQuestionManDTOS);
+        return response;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/questions-man")
+    public Response updateSurveyQuestionMan(@RequestBody SurveyQuestionManDTO surveyQuestionManDTO) throws JsonProcessingException {
+        Response response = new Response(HttpStatus.OK.value());
+        try {
+            SurveyQuestionManDTO surveyQuestionManDTO1;
+            surveyQuestionManDTO1 = surveyQuestionManService.updateSurveyQuestionMan(surveyQuestionManDTO);
+            if (Objects.nonNull(surveyQuestionManDTO1.getErrorCode())) {
+                response.setStatus(surveyQuestionManDTO1.getErrorCode());
+                response.setMessage(surveyQuestionManDTO1.getMessage());
+            }
+        } catch (JsonProcessingException e) {
+            response.setMessage(ErrorCode.JSON_PROCESSING_EXCEPTION.getDescription());
+            response.setStatus(ErrorCode.JSON_PROCESSING_EXCEPTION.getValue());
+        }
+
         return response;
     }
 }
