2be479990e8f9ad2a7eb8a6b9385323a7647d46a
Thieu Tu <tu.thieuthanh@gmail.com>
2018-08-16 14:06:12 +0700

    override method: updateSurveyQuestionMan

--- src/main/java/com/higgsup/kpi/service/impl/SurveyQuestionManServiceImpl.java
+++ src/main/java/com/higgsup/kpi/service/impl/SurveyQuestionManServiceImpl.java
@@ -1,7 +1,10 @@
 package com.higgsup.kpi.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.higgsup.kpi.dto.SurveyQuestionManDTO;
 import com.higgsup.kpi.entity.KpiSurveyQuestionMan;
+import com.higgsup.kpi.glossary.ErrorCode;
 import com.higgsup.kpi.repository.KpiSurveyQuestionManRepo;
 import com.higgsup.kpi.service.SurveyQuestionManService;
 import org.springframework.beans.BeanUtils;
@@ -11,6 +14,7 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -22,6 +26,27 @@ public class SurveyQuestionManServiceImpl implements SurveyQuestionManService {
     public List<SurveyQuestionManDTO> getAllQuestion() {
         List<KpiSurveyQuestionMan> questions = (List<KpiSurveyQuestionMan>) surveyQuestionManRepo.findAll();
         return convertKpiSurveyQuestionManToDTO(questions);
+    }
+
+    @Override
+    public SurveyQuestionManDTO updateSurveyQuestionMan(SurveyQuestionManDTO surveyQuestionManDTO) throws JsonProcessingException {
+        Integer id = surveyQuestionManDTO.getId();
+        SurveyQuestionManDTO surveyQuestionManDTO1 = new SurveyQuestionManDTO();
+
+        if (surveyQuestionManRepo.findById(id) == null) {
+            surveyQuestionManDTO1.setMessage(ErrorCode.NOT_FIND.getDescription());
+            surveyQuestionManDTO1.setErrorCode(ErrorCode.NOT_FIND.getValue());
+        } else {
+            Optional<KpiSurveyQuestionMan> kpiSurveyQuestionManOptional = surveyQuestionManRepo.findById(id);
+            if (kpiSurveyQuestionManOptional.isPresent()) {
+                KpiSurveyQuestionMan kpiSurveyQuestionMan = kpiSurveyQuestionManOptional.get();
+                surveyQuestionManDTO.setId(kpiSurveyQuestionMan.getId());
+                BeanUtils.copyProperties(surveyQuestionManDTO, kpiSurveyQuestionMan);
+                surveyQuestionManRepo.save(kpiSurveyQuestionMan);
+            }
+        }
+
+        return surveyQuestionManDTO1;
     }
 
     private List<SurveyQuestionManDTO> convertKpiSurveyQuestionManToDTO(List<KpiSurveyQuestionMan> questions) {
