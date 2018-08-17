22554231a6f6f755574edbd736fba1211b0cf1d0
NgThuy <ngothuy05111998@gmail.com>
2018-08-17 14:11:17 +0700

    edit

--- src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
+++ src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
@@ -4,6 +4,8 @@ public final class ErrorMessage {
     public final static String NOT_FIND_USER = "not find user";
     public final static String PARAMETERS_NAME_IS_NOT_VALID = "parameter name is not valid";
     public final static String NOT_FIND_SEMINAR = "not find seminar";
+    public final static String NOT_FIND_GROUP= "not find group";
+    public final static String PARAMETERS_NAME_PROJECT_EXISTS = "parameters name project exists";
     public final static String ALREADY_EXIT_SEMINAR = "seminar already exist!";
     public final static String POINT_HOST_IS_NOT_VALIDATE = "parameter point host  is not valid";
     public final static String POINT_MEMBER_IS_NOT_VALIDATE = "parameter point member  is not valid";
@@ -14,6 +16,8 @@ public final class ErrorMessage {
     public final static String ALREADY_CREATED = "already created";
     public final static String GROUP_ALREADY_EXISTS ="group name already exists";
     public final static String NOT_FIND_GROUP_TYPE ="group type does not exist";
+    public final static String DATA_EXIST_WITH_USER_NAMWE = "user name exist";
+    public final static String PARAMETERS_ROLES_IS_EMPTY = "parameterrole is empty";
     public final static String PARAMETERS_MIN_NUMBER_OF_SESSIONS_IS_NOT_VALID ="min number of sessions is not valid";
     public final static String PARAMETERS_POINT_IS_NOT_VALID="parameter point is not valid";
 }
