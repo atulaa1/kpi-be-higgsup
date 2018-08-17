bea754b4de99767bdd1799a518645fe2f1cd5bc0
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-08-17 10:17:04 +0700

    Update support config

--- src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
+++ src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
@@ -10,7 +10,6 @@ public final class ErrorMessage {
     public final static String NOT_FIND_GROUP= "not find group";
     public final static String PARAMETERS_NAME_PROJECT_EXISTS = "parameters name project exists";
     public final static String NOT_FILLING_ALL_INFORMATION = "not filling all information";
-    public final static String PARAMETERS_ROLES_IS_EMPTY = "parameterrole is empty";
-
-    public final static String DATA_EXIST_WITH_USER_NAMWE = "user name exist";
+    public final static String PARAMETERS_ROLES_IS_EMPTY = "parameter role is empty";
+    public final static String DATA_EXIST_WITH_USER_NAME = "user name exist";
 }
