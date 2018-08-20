122b92aa507e7adb8768193d8b05b9b9f94bac40
NgThuy <ngothuy05111998@gmail.com>
2018-08-20 17:52:16 +0700

    edit error code , error message , utilsvalidate.

--- src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
+++ src/main/java/com/higgsup/kpi/glossary/ErrorMessage.java
@@ -1,13 +1,7 @@
 package com.higgsup.kpi.glossary;
 
 public final class ErrorMessage {
-   // public final static String NOT_FIND_SEMINAR = "not find seminar";
     public final static String ALREADY_EXIT_SEMINAR = "seminar already exist!";
-   /* public final static String POINT_HOST_IS_NOT_VALIDATE = "parameter point host  is not valid";
-    public final static String POINT_MEMBER_IS_NOT_VALIDATE = "parameter point member  is not valid";
-    public final static String POINT_LISTENER_IS_NOT_VALIDATE = "parameter point listener  is not valid";
-    public final static String POINT_HOST_NOT_LARGER_THAN_POINT_MEMBER = "point host not larger than point member";
-    public final static String POINT_MEMBER_NOT_LARGER_THAN_POINT_LISTENER ="point member not larger than point listener";*/
     public final static String NOT_FIND_USER = "not find user";
     public final static String PARAMETERS_NAME_IS_NOT_VALID = "parameter name is not valid";
     public final static String NOT_FILLING_ALL_INFORMATION = "not filling all information";
