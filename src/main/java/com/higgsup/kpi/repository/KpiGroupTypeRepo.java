973421ddfdb64c40ee643b533e2ca94decaece30
tuanhiep <nguyennhutuanhiep@gmail.com>
2018-09-06 15:44:18 +0700

    Sort GroupType based on alphabet

--- src/main/java/com/higgsup/kpi/repository/KpiGroupTypeRepo.java
+++ src/main/java/com/higgsup/kpi/repository/KpiGroupTypeRepo.java
@@ -1,8 +1,12 @@
 package com.higgsup.kpi.repository;
 
 import com.higgsup.kpi.entity.KpiGroupType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface KpiGroupTypeRepo extends CrudRepository<KpiGroupType, Integer> {
+import java.util.List;
 
+public interface KpiGroupTypeRepo extends CrudRepository<KpiGroupType, Integer> {
+    @Query("SELECT g from KpiGroupType g order by g.name")
+    List<KpiGroupType> findAllGroupTypeSortedName();
 }
