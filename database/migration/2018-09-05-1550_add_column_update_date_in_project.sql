1fa41eb0cb7614aadf166af3e8bfa3dc23dc3c2a
thanhlv <lethanh9398@gmail.com>
2018-09-06 18:21:34 +0700

    change name comment

--- database/migration/2018-09-05-1550_add_column_update_date_in_project.sql
+++ database/migration/2018-09-05-1550_add_column_update_date_in_project.sql
@@ -1,7 +1,7 @@
 /**
 date: 2018-09-05 15:50
 author: ThanhLV
-purpose: add column update_date in project
+purpose: add column updated_date in project
 **/
 
 ALTER TABLE kpi_project ADD updated_date DATETIME NULL;
