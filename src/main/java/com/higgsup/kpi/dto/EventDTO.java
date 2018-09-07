9ae58813dfc00e5def9643fdd989db9217856546
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-07 16:00:31 +0700

    create EvenDTO

---
+++ src/main/java/com/higgsup/kpi/dto/EventDTO.java
@@ -0,0 +1,133 @@
+package com.higgsup.kpi.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.higgsup.kpi.entity.KpiGroup;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class EventDTO<T> extends BaseDTO {
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private Integer status;
+
+    private String address;
+
+    @JsonFormat(pattern = "dd-MM-yyy HH:mm")
+    private Timestamp createdDate;
+
+    @JsonFormat(pattern = "dd-MM-yyy HH:mm")
+    private Timestamp updatedDate;
+
+    @JsonFormat(pattern = "dd-MM-yyy HH:mm")
+    private Timestamp beginDate;
+
+    @JsonFormat(pattern = "dd-MM-yyy HH:mm")
+    private Timestamp endDate;
+
+    private KpiGroup group;
+
+    private List<EventUserDTO> participants;
+
+    private T additionalConfig;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Timestamp getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Timestamp updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public Timestamp getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Timestamp beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+
+    public KpiGroup getGroup() {
+        return group;
+    }
+
+    public void setGroup(KpiGroup group) {
+        this.group = group;
+    }
+
+    public List<EventUserDTO> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<EventUserDTO> participants) {
+        this.participants = participants;
+    }
+
+    public T getAdditionalConfig() {
+        return additionalConfig;
+    }
+
+    public void setAdditionalConfig(T additionalConfig) {
+        this.additionalConfig = additionalConfig;
+    }
+}
