f40ce8abc870fdd1187f8088938cdd1bbcd1fc64
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-07 16:48:24 +0700

    add base API

--- src/main/java/com/higgsup/kpi/controller/EventController.java
+++ src/main/java/com/higgsup/kpi/controller/EventController.java
@@ -1,4 +1,10 @@
 package com.higgsup.kpi.controller;
 
+import com.higgsup.kpi.configure.BaseConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(BaseConfiguration.BASE_API_URL +"/events")
 public class EventController {
 }
