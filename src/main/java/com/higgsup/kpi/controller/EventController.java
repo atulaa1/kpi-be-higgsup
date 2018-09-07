7147eee13cb2b28a082433bd64af0c29cab2f0ee
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-07 17:28:52 +0700

    edit controller

--- src/main/java/com/higgsup/kpi/controller/EventController.java
+++ src/main/java/com/higgsup/kpi/controller/EventController.java
@@ -1,10 +1,15 @@
 package com.higgsup.kpi.controller;
 
 import com.higgsup.kpi.configure.BaseConfiguration;
+import com.higgsup.kpi.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(BaseConfiguration.BASE_API_URL +"/events")
 public class EventController {
+
+    @Autowired
+    private EventService eventService;
 }
