5a32c436d615b79517b76b0983edc50356010459
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-08 11:48:47 +0700

    edit eventController

--- src/main/java/com/higgsup/kpi/controller/EventController.java
+++ src/main/java/com/higgsup/kpi/controller/EventController.java
@@ -1,10 +1,21 @@
 package com.higgsup.kpi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.higgsup.kpi.configure.BaseConfiguration;
+import com.higgsup.kpi.dto.EventClubDetail;
+import com.higgsup.kpi.dto.EventDTO;
+import com.higgsup.kpi.dto.Response;
+import com.higgsup.kpi.glossary.ErrorCode;
 import com.higgsup.kpi.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping(BaseConfiguration.BASE_API_URL +"/events")
@@ -12,4 +23,23 @@ public class EventController {
 
     @Autowired
     private EventService eventService;
+
+    @PreAuthorize("hasAnyRole('MAN','EMPLOYEE')")
+    @PostMapping("/club")
+    public Response createClub(@RequestBody EventDTO<EventClubDetail> eventDTO){
+        Response<EventDTO> response = new Response<>(HttpStatus.OK.value());
+        try {
+            EventDTO eventDTOResponse = eventService.createClubEvent(eventDTO);
+            if (Objects.nonNull(eventDTOResponse.getErrorCode())) {
+                response.setStatus(eventDTOResponse.getErrorCode());
+                response.setMessage(eventDTOResponse.getMessage());
+            } else {
+                response.setData(eventDTOResponse);
+            }
+        } catch (JsonProcessingException e) {
+            response.setMessage(ErrorCode.JSON_PROCESSING_EXCEPTION.getDescription());
+            response.setStatus(ErrorCode.JSON_PROCESSING_EXCEPTION.getValue());
+        }
+        return response;
+    }
 }
