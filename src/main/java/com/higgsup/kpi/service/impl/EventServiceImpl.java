34d2c23a84c4bb9ef69fa96c80d2dbebf1a978bd
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-08 11:45:40 +0700

    create API: create a club event

--- src/main/java/com/higgsup/kpi/service/impl/EventServiceImpl.java
+++ src/main/java/com/higgsup/kpi/service/impl/EventServiceImpl.java
@@ -1,16 +1,23 @@
 package com.higgsup.kpi.service.impl;
 
 import com.higgsup.kpi.repository.KpiEventRepo;
-import com.higgsup.kpi.repository.KpiEventUserRepo;
+import com.higgsup.kpi.repository.KpiGroupRepo;
 import com.higgsup.kpi.service.EventService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Optional;
 
 @Service
 public class EventServiceImpl implements EventService {
 
     @Autowired
     private KpiEventRepo kpiEventRepo;
+
+    @Autowired
+    private KpiGroupRepo kpiGroupRepo;
 
     @Autowired
     private KpiEventUserRepo kpiEventUserRepo;
