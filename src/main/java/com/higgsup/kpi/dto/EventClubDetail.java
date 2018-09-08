072d471c59932839903677c139dd44b98da613c5
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-08 11:16:40 +0700

    edit EventClubDetail class

--- src/main/java/com/higgsup/kpi/dto/EventClubDetail.java
+++ src/main/java/com/higgsup/kpi/dto/EventClubDetail.java
@@ -1,17 +1,7 @@
 package com.higgsup.kpi.dto;
 
 public class EventClubDetail {
-    private String participant;
-
     private String pointInfo;
-
-    public String getParticipant() {
-        return participant;
-    }
-
-    public void setParticipant(String participant) {
-        this.participant = participant;
-    }
 
     public String getPointInfo() {
         return pointInfo;
