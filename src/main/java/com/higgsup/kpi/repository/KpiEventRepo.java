08ab04dd1d3f70d1310065a077a65be83354d5e7
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-08 11:49:12 +0700

    add findbyName method

--- src/main/java/com/higgsup/kpi/repository/KpiEventRepo.java
+++ src/main/java/com/higgsup/kpi/repository/KpiEventRepo.java
@@ -1,8 +1,10 @@
 package com.higgsup.kpi.repository;
 
+import com.higgsup.kpi.dto.EventDTO;
 import com.higgsup.kpi.entity.KpiEvent;
 import org.springframework.data.repository.CrudRepository;
 
 public interface KpiEventRepo extends CrudRepository<KpiEvent, Integer> {
+    EventDTO findByName(String name);
 
 }
