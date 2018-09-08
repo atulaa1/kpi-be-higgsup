499b57bd547e849af05bbb5eb2df9875189c1186
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-08 11:50:27 +0700

    create createClubEvent method

--- src/main/java/com/higgsup/kpi/service/EventService.java
+++ src/main/java/com/higgsup/kpi/service/EventService.java
@@ -1,5 +1,9 @@
 package com.higgsup.kpi.service;
 
-public interface EventService {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.higgsup.kpi.dto.EventClubDetail;
+import com.higgsup.kpi.dto.EventDTO;
 
+public interface EventService {
+    EventDTO createClubEvent(EventDTO<EventClubDetail> eventDTO) throws JsonProcessingException;
 }
