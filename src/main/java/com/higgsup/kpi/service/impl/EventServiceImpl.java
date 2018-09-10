ab38f2c0daf2fca7a10fc3104a7f306eaf415da7
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-10 15:06:34 +0700

    edit eventService

--- src/main/java/com/higgsup/kpi/service/impl/EventServiceImpl.java
+++ src/main/java/com/higgsup/kpi/service/impl/EventServiceImpl.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.higgsup.kpi.dto.EventClubDetail;
 import com.higgsup.kpi.dto.EventDTO;
+import com.higgsup.kpi.dto.EventUserDTO;
 import com.higgsup.kpi.entity.KpiEvent;
 import com.higgsup.kpi.entity.KpiEventUser;
+import com.higgsup.kpi.entity.KpiEventUserPK;
 import com.higgsup.kpi.entity.KpiGroup;
 import com.higgsup.kpi.glossary.ErrorCode;
 import com.higgsup.kpi.glossary.ErrorMessage;
@@ -13,11 +15,15 @@ import com.higgsup.kpi.repository.KpiEventRepo;
 import com.higgsup.kpi.repository.KpiEventUserRepo;
 import com.higgsup.kpi.repository.KpiGroupRepo;
 import com.higgsup.kpi.service.EventService;
+import com.higgsup.kpi.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -32,14 +38,18 @@ public class EventServiceImpl implements EventService {
     @Autowired
     private KpiEventUserRepo kpiEventUserRepo;
 
+    @Autowired
+    UserService userService;
+
     @Override
+    @Transactional
     public EventDTO createClub(EventDTO<EventClubDetail> eventDTO) throws JsonProcessingException {
         EventDTO<EventClubDetail> validatedEventDTO = new EventDTO<>();
 
         if (kpiEventRepo.findByName(eventDTO.getName()) == null) {
             KpiEvent kpiEvent = new KpiEvent();
-            ObjectMapper mapper = new ObjectMapper();
 
+            ObjectMapper mapper = new ObjectMapper();
             if (validateClub(eventDTO, validatedEventDTO)) {
                 String clubJson = mapper.writeValueAsString(eventDTO.getAdditionalConfig());
                 BeanUtils.copyProperties(eventDTO, kpiEvent);
@@ -52,12 +62,13 @@ public class EventServiceImpl implements EventService {
                     kpiEvent.setGroup(groupOptional.get());
                     kpiEvent = kpiEventRepo.save(kpiEvent);
 
-                    for (KpiEventUser participant : kpiEvent.getParticipants()) {
-                        kpiEventUserRepo.save(participant);
-                    }
+                    List<KpiEventUser> eventUsers = convertEventUsersToEntity(kpiEvent, eventDTO.getEventUserList());
+                    kpiEventUserRepo.saveAll(eventUsers);
+
                     BeanUtils.copyProperties(kpiEvent, validatedEventDTO);
                     validatedEventDTO.setGroup(eventDTO.getGroup());
                     validatedEventDTO.setAdditionalConfig(eventDTO.getAdditionalConfig());
+                    validatedEventDTO.setEventUserList(eventDTO.getEventUserList());
                 } else {
                     validatedEventDTO.setMessage(ErrorMessage.NOT_FIND_GROUP);
                     validatedEventDTO.setErrorCode(ErrorCode.NOT_FIND.getValue());
@@ -71,6 +82,24 @@ public class EventServiceImpl implements EventService {
         return validatedEventDTO;
     }
 
+    private List<KpiEventUser> convertEventUsersToEntity(KpiEvent kpiEvent, List<EventUserDTO> eventUserList) {
+        List<KpiEventUser> kpiEventUsers = new ArrayList<>();
+        for (EventUserDTO eventUserDTO : eventUserList) {
+            KpiEventUser kpiEventUser = new KpiEventUser();
+            KpiEventUserPK kpiEventUserPK = new KpiEventUserPK();
+            userService.registerUser(eventUserDTO.getUser().getUsername());
+
+            kpiEventUserPK.setEventId(kpiEvent.getId());
+            kpiEventUserPK.setUserName(eventUserDTO.getUser().getUsername());
+            kpiEventUser.setKpiEventUserPK(kpiEventUserPK);
+
+            kpiEventUser.setType(eventUserDTO.getType());
+            kpiEventUsers.add(kpiEventUser);
+
+        }
+        return kpiEventUsers;
+    }
+
     private Boolean validateClub(EventDTO<EventClubDetail> eventDTO, EventDTO validatedEventDTO) {
         Boolean validate = false;
 
@@ -80,7 +109,7 @@ public class EventServiceImpl implements EventService {
         } else if (eventDTO.getAdditionalConfig().getPointInfo() == null) {
             validatedEventDTO.setMessage(ErrorMessage.POINT_INFO_CAN_NOT_NULL);
             validatedEventDTO.setErrorCode(ErrorCode.NOT_NULL.getValue());
-        } else if (eventDTO.getParticipants().size() == 0) {
+        } else if (eventDTO.getEventUserList().size() == 0) {
             validatedEventDTO.setMessage(ErrorMessage.LIST_OF_PARTICIPANTS_CAN_NOT_NULL);
             validatedEventDTO.setErrorCode(ErrorCode.NOT_NULL.getValue());
         } else if (eventDTO.getBeginDate() == null) {
