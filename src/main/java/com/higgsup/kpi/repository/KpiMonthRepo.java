0f6634e6f369c3260cdc192360ffa60e73113a17
thanhlv <lethanh9398@gmail.com>
2018-09-11 17:16:22 +0700

    change format

--- src/main/java/com/higgsup/kpi/repository/KpiMonthRepo.java
+++ src/main/java/com/higgsup/kpi/repository/KpiMonthRepo.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface KpiMonthRepo extends CrudRepository<KpiMonth, Integer> {
-    @Query(value = "SELECT * FROM kpi_month WHERE MONTH(month) = MONTH(CURRENT_DATE()) OR MONTH(month) = MONTH(CURRENT_DATE()) - 1 AND YEAR(month) = YEAR(CURRENT_DATE()) AND DATE_FORMAT( NOW(), '%H:%i' ) <= '16:00:00' AND Day(month) <= 10 ORDER BY month DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM kpi_month WHERE MONTH(month) = MONTH(CURRENT_DATE()) OR MONTH(month) = MONTH(CURRENT_DATE()) - 1 AND YEAR(month) = YEAR(CURRENT_DATE()) AND DATE_FORMAT( NOW(), '%H:%i' ) <= '16:00' AND Day(month) <= 10 ORDER BY month DESC LIMIT 1", nativeQuery = true)
     Optional<KpiMonth> findByMonthCurrent();
 }
