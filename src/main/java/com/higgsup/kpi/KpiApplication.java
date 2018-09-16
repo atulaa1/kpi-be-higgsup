1da217de1ff3f02693581bd65a55149bb7f52234
Thieu Tu <tu.thieuthanh@gmail.com>
2018-09-16 22:49:46 +0700

    integrate logger into kpi

--- src/main/java/com/higgsup/kpi/KpiApplication.java
+++ src/main/java/com/higgsup/kpi/KpiApplication.java
@@ -1,5 +1,7 @@
 package com.higgsup.kpi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,12 +10,15 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class KpiApplication extends SpringBootServletInitializer {
 
+    private static Logger logger = LogManager.getLogger(KpiApplication.class);
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(KpiApplication.class);
     }
 
     public static void main(String[] args) {
+        logger.info("Starting Spring Boot application..");
         SpringApplication.run(KpiApplication.class, args);
     }
 }
