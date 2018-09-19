404a3a6dab2131978841d3002fd675689a126302
tutt <thieuthanhtu@gmail.com>
2018-09-19 16:34:59 +0700

    delete unused libraries

--- src/main/java/com/higgsup/kpi/controller/EventController.java
+++ src/main/java/com/higgsup/kpi/controller/EventController.java
@@ -6,8 +6,6 @@ import com.higgsup.kpi.dto.*;
 import com.higgsup.kpi.glossary.ErrorCode;
 import com.higgsup.kpi.glossary.ErrorMessage;
 import com.higgsup.kpi.service.EventService;
-import javafx.application.Application;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
