14e2b16726411b353b0fefafa076a4e5ab43e0bf
tuanhiep97 <nguyennhutuanhiep@gmail.com>
2018-09-20 16:39:03 +0700

    Get all team building event

--- src/main/java/com/higgsup/kpi/glossary/EventUserType.java
+++ src/main/java/com/higgsup/kpi/glossary/EventUserType.java
@@ -3,11 +3,7 @@ package com.higgsup.kpi.glossary;
 public enum EventUserType {
     HOST(1, "host"),
     MEMBER(2, "member"),
-    LISTEN(3, "listen"),
-    ORGANIZER(4, "organizer"),
-    FIRST_PLACE(5, "first place"),
-    SECOND_PLACE(6, "second place"),
-    THIRD_PLACE(7, "third place");
+    LISTEN(3, "listen");
 
     private Integer value;
     private String content;
