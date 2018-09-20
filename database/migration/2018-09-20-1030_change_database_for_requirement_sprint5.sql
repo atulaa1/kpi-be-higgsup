3bd0cd46e5cc4533b1f1bb1dc789f2457713ceed
thanhlv <lethanh9398@gmail.com>
2018-09-20 11:09:58 +0700

    change name

--- database/migration/2018-09-20-1030_change_database_for_requirement_sprint5.sql
+++ database/migration/2018-09-20-1030_change_database_for_requirement_sprint5.sql
@@ -7,12 +7,12 @@ purpose: add new column status for kpi_event_user table
 **/
 CREATE TABLE `kpi_seminar_survey` (
   `id` int(10) PRIMARY KEY AUTO_INCREMENT,
-  `assessor_username` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `evaluating_username` varchar(50) CHARACTER SET utf8 NOT NULL,
   `event_id` int(11),
   `evaluated_username` varchar(50) CHARACTER SET utf8 NOT NULL,
   `rating` varchar(50) CHARACTER SET utf8 NOT NULL,
 
-  FOREIGN KEY (assessor_username) REFERENCES kpi_event_user(user_name),
+  FOREIGN KEY (evaluating_username) REFERENCES kpi_event_user(user_name),
   FOREIGN KEY (evaluated_username) REFERENCES kpi_event_user(user_name),
   FOREIGN KEY (event_id) REFERENCES kpi_event(id)
 
