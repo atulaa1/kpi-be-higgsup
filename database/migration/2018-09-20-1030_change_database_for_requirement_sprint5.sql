dd8572b453bf23fc46a7343ccb03d8782d4a6610
thanhlv <lethanh9398@gmail.com>
2018-09-20 10:48:16 +0700

    change database fot sprint 5

---
+++ database/migration/2018-09-20-1030_change_database_for_requirement_sprint5.sql
@@ -0,0 +1,22 @@
+/**
+date: 2018-09-20 10:30
+author: ThanhLV
+purpose: create table kpi_seminar_survey
+purpose: add new column status for kpi_event_user table
+
+**/
+CREATE TABLE `kpi_seminar_survey` (
+  `id` int(10) PRIMARY KEY AUTO_INCREMENT,
+  `assessor_username` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `event_id` int(11),
+  `evaluated_username` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `rating` varchar(50) CHARACTER SET utf8 NOT NULL,
+
+  FOREIGN KEY (assessor_username) REFERENCES kpi_event_user(user_name),
+  FOREIGN KEY (evaluated_username) REFERENCES kpi_event_user(user_name),
+  FOREIGN KEY (event_id) REFERENCES kpi_event(id)
+
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE kpi_event_user ADD status TINYINT DEFAULT 0 NULL;
