dc2e7ee46cd40d76d44a40e45535fe350046813c
thanhlv <lethanh9398@gmail.com>
2018-09-21 16:59:46 +0700

    change name

--- database/migration/2018-09-20-1656_add_table_support.sql
+++ database/migration/2018-09-20-1656_add_table_support.sql
@@ -8,7 +8,7 @@ purpose: create table kip_support
 CREATE TABLE kpi_support
 (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  task VARCHAR(500),
+  task_name VARCHAR(500),
   task_point FLOAT,
   created_date      DATETIME     DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
