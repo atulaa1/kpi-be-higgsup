4018d275f293aeb2266863a66090e306ab593d29
tutt <thieuthanhtu@gmail.com>
2018-10-09 13:52:09 +0700

    create Evaluation Service

--- src/main/java/com/higgsup/kpi/service/EvaluationService.java
+++ src/main/java/com/higgsup/kpi/service/EvaluationService.java
@@ -1,5 +1,10 @@
 package com.higgsup.kpi.service;
 
+import com.higgsup.kpi.dto.EmployeeEvaluationDTO;
+
+import java.io.IOException;
+
 public interface EvaluationService {
+    EmployeeEvaluationDTO createEmployeeEvaluation(EmployeeEvaluationDTO employeeEvaluationDTO) throws IOException;
 
 }
--- src/main/java/com/higgsup/kpi/service/impl/EvaluationServiceImpl.java
+++ src/main/java/com/higgsup/kpi/service/impl/EvaluationServiceImpl.java
@@ -1,7 +1,15 @@
 package com.higgsup.kpi.service.impl;
 
+import com.higgsup.kpi.dto.EmployeeEvaluationDTO;
+import com.higgsup.kpi.service.EvaluationService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EvaluationServiceImpl {
+public class EvaluationServiceImpl implements EvaluationService {
+    @Override
+    public EmployeeEvaluationDTO createEmployeeEvaluation(EmployeeEvaluationDTO employeeEvaluationDTO) {
+
+
+        return null;
+    }
 }
